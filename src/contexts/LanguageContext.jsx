f00dd2232b5039fd43638d9d316506970bf23dd2
Zerrin Unalan <zerunalan@gmail.com>
2024-05-16 14:27:52 +0300

    LanguageContext düzenlendi.

--- src/contexts/LanguageContext.jsx
+++ src/contexts/LanguageContext.jsx
@@ -1,13 +1,45 @@
-import { useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import useLocalStorage from "../hooks/useLocalStorage";
 import axios from "axios";
+import data from "../mocks/data.json";
 
-const fetchData = async () => {
-  try {
-    const response = await axios.post("https://reqres.in/api/workintech", data);
-    setApiData(response.data[language]);
-    setLoading(false);
-  } catch (error) {
-    console.error(error);
-    setLoading(false);
+export const LanguageContext = createContext();
+
+export const LanguageProvider = ({ children }) => {
+  const [language, setLanguage] = useLocalStorage("language", "en");
+  const [apiData, setApiData] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.post(
+        "https://reqres.in/api/workintech",
+        data
+      );
+      setApiData(response.data[language]);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [language]);
+
+  const toggleLanguage = () => {
+    const newLang = language === "en" ? "tr" : "en";
+    setLanguage(newLang);
+  };
+
+  if (loading) {
+    return <div>Loading...</div>;
   }
+
+  return (
+    <LanguageContext.Provider value={{ apiData, toggleLanguage }}>
+      {children}
+    </LanguageContext.Provider>
+  );
 };
