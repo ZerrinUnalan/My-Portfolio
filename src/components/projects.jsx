76c29789e30cf7cb8f86130ec97f929940ee37b6
Zerrin Unalan <zerunalan@gmail.com>
2024-07-12 14:53:21 +0300

    Updated

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -14,17 +14,17 @@ const Projects = () => {
           {projects.items.map((item) => {
             return (
               <div className="w-3/5 md:w-1/4">
-                <img className="w-full" src={item.image} alt={item.title} />
+                <img className="w-full " src={item.image} alt={item.title} />
                 <h2 className="text-[#4338CA] dark:text-[#CFCBFF] font-medium text-3xl my-2">
                   {item.title}
                 </h2>
                 <p className="text-[#6B7280] dark:text-white text-xs">
                   {item.description}
                 </p>
-                <div className="flex my-2 gap-2">
+                <div className="flex my-2 gap-2 flex flex-wrap justify-start">
                   {item.technologies.map((i) => {
                     return (
-                      <p className="text-[#3730A3] text-xs font-medium border border-[#3730A3] dark:text-[#8F88FF] dark:border-[#8F88FF] py-1 px-2 rounded">
+                      <p className="text-[#3730A3] text-xs font-medium border border-[#3730A3] dark:text-[#8F88FF] dark:border-[#8F88FF] py-1 px-2 rounded ">
                         {i}
                       </p>
                     );
